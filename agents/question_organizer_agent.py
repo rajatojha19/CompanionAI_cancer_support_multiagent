53d5f2b8f7ee514b983e33b8f51e1cc6636b941a
Rajat Ojha <rajatojha2050@gmail.com>
2026-08-01 18:08:52 +0530

    Move QuestionOrganizerAgent into agents package

---
+++ agents/question_organizer_agent.py
@@ -0,0 +1,7 @@
+import logging
+from typing import List, Optional
+
+from services.gemini_service import GeminiClient
+from agents.question_organizer_agent import QuestionOrganizerAgent
+
+logger = logging.getLogger("CancerSupportCompanion")
